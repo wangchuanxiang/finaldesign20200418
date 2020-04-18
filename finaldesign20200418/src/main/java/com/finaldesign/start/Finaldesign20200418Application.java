cc18f807a84fc45a18897ab5530c29e29168f437
wcx80 <wcx80@DESKTOP-N62MA58>
2020-04-18 17:20:12 +0800

    提交代码结构

---
+++ finaldesign20200418/src/main/java/com/finaldesign/start/Finaldesign20200418Application.java
@@ -0,0 +1,13 @@
+package com.finaldesign.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finaldesign20200418Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finaldesign20200418Application.class, args);
+	}
+
+}
---
+++ finaldesign20200418/src/test/java/com/finaldesign/start/Finaldesign20200418ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finaldesign.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finaldesign20200418ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
