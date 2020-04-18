c54c79149dace6b4f5be1a53f10105ca20d89751
wcx80 <wcx80@DESKTOP-N62MA58>
2020-04-18 23:40:40 +0800

    需要解决问题：java.lang.NoClassDefFoundError: javax/inject/Provider

--- finaldesign20200418/src/main/java/com/finaldesign/start/Finaldesign20200418Application.java
+++ finaldesign20200418/src/main/java/com/finaldesign/start/Finaldesign20200418Application.java
@@ -1,8 +1,12 @@
 package com.finaldesign.start;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@MapperScan("com.finaldesign.dao.mapper")
+@ComponentScan("com.finaldesign")
 @SpringBootApplication
 public class Finaldesign20200418Application {
 
